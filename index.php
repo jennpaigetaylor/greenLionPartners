496951827146af821ad9716d8cd2658b464cc4c5
jptaylor3 <jenniferptaylor3@gmail.com>
2015-12-06 20:29:26 -0500

    index.html to index.php

--- index.php
+++ index.php
@@ -290,7 +290,8 @@
             </div>
             <div class="row">
                 <div class="col-lg-12 text-center">
-                    <form name="sentMessage" id="contactForm" novalidate>
+                    <!-- <form name="sentMessage" id="contactForm" novalidate> -->
+                    <form action="MAILTO:contact@greenlionpartners.com" method="post" enctype="text/plain">
                         <div class="row">
                             <div class="col-md-4 col-md-offset-4">
                                 <div class="form-group">
