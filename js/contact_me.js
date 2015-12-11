4d19008961bffd86ef81759ee4eb4a74f6256a45
jptaylor3 <jenniferptaylor3@gmail.com>
2015-12-11 15:20:29 -0500

    new contact@greenlion email w formspree

--- js/contact_me.js
+++ js/contact_me.js
@@ -18,7 +18,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "//formspree.io/jenniferptaylor3@gmail.com",
+                url: "//formspree.io/contact@greenlionpartners.com",
                 type: "POST",
                 data: {
                     name: name,
